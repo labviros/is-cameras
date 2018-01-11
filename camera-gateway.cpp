4edcda73a41ae0d21d484a8dc7f576f65f601b4b
felippe <mendonca.felippe@gmail.com>
2018-01-11 13:10:13 -0200

    opencv parallelism option added.

--- camera-gateway.cpp
+++ camera-gateway.cpp
@@ -3,30 +3,27 @@
 #include "ptgrey-driver.hpp"
 
 int main(int argc, char** argv) {
-  std::string uri;
-  std::string camera_ip;
+  std::string uri, camera_ip, zipkin_host;
   unsigned int id;
-  int packet_delay;
-  int packet_size;
-  std::string zipkin_host;
-  int zipkin_port;
+  int packet_delay, packet_size, zipkin_port, parallelism;
 
   is::po::options_description opts("Options");
-  opts.add_options()("uri,u", is::po::value<std::string>(&uri)->required(), "amqp broker uri");
-  opts.add_options()("camera-ip,c", is::po::value<std::string>(&camera_ip)->required(),
-                     "camera ip address");
-  opts.add_options()("id,i", is::po::value<unsigned int>(&id)->default_value(0), "camera id");
-  opts.add_options()("packet-delay,d", is::po::value<int>(&packet_delay)->default_value(6000),
-                     "packet delay [0~6250]");
-  opts.add_options()("packet-size,s", is::po::value<int>(&packet_size)->default_value(6500),
-                     "packet delay [576~9000]");
-  opts.add_options()("zipkin-host,z",
-                     is::po::value<std::string>(&zipkin_host)->default_value("zipkin.default"),
-                     "zipkin hostname");
-  opts.add_options()("zipkin-port,p", is::po::value<int>(&zipkin_port)->default_value(9411),
-                     "zipkin port");
+  auto&& opt_add = opts.add_options();
+  opt_add("uri,u", is::po::value<std::string>(&uri)->required(), "amqp broker uri");
+  opt_add("camera-ip,c", is::po::value<std::string>(&camera_ip)->required(), "camera ip address");
+  opt_add("id,i", is::po::value<unsigned int>(&id)->default_value(0), "camera id");
+  opt_add("packet-delay,d", is::po::value<int>(&packet_delay)->default_value(6000),
+          "packet delay [0~6250]");
+  opt_add("packet-size,s", is::po::value<int>(&packet_size)->default_value(6500),
+          "packet delay [576~9000]");
+  opt_add("zipkin-host,z",
+          is::po::value<std::string>(&zipkin_host)->default_value("zipkin.default"),
+          "zipkin hostname");
+  opt_add("zipkin-port,p", is::po::value<int>(&zipkin_port)->default_value(9411), "zipkin port");
+  opt_add("parallelism", is::po::value<int>(&parallelism)->default_value(0), "number of threads");
   is::parse_program_options(argc, argv, opts);
 
+  cv::setNumThreads(parallelism);
   auto driver = std::make_unique<is::camera::PtgreyDriver>();
   driver->connect(camera_ip);
   driver->set_packet_delay(packet_delay);
