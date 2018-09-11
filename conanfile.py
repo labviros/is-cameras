69c51d6f2bf2e65bc1cc00e61eec6e422b75ebde
Felippe <mendonca.felippe@gmail.com>
2018-09-11 10:44:22 -0300

    bug fixed when using flycapture and spinaker driver - new conanfile style (python) - re-released docker image 1.4

---
+++ conanfile.py
@@ -0,0 +1,51 @@
+from conans import ConanFile, CMake, tools
+
+
+class IsCamerasConan(ConanFile):
+    name = "is-cameras"
+    version = "1.0.0"
+    license = "MIT"
+    url = ""
+    description = ""
+    settings = "os", "compiler", "build_type", "arch"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+        "build_tests": [True, False],
+    }
+    default_options = "shared=False", "fPIC=True", "build_tests=False"
+    generators = "cmake", "cmake_find_package", "cmake_paths"
+    requires = (
+        "opencv/3.3.1@is/stable",
+        "is-wire/1.1.4@is/stable",
+        "is-msgs/1.1.8@is/stable",
+        "zipkin-cpp-opentracing/0.3.1@is/stable",
+        "spinnaker/1.10.0.31@is/stable",
+        "flycapture2/2.12.3.31@is/stable",
+        "boost/1.67.0@conan/stable",
+    )
+    exports_sources = "*"
+
+    # def build_requirements(self):
+    #     if self.options.build_tests:
+    #         self.build_requires("gtest/1.8.0@bincrafters/stable")
+
+    def configure(self):
+        self.options["is-msgs"].shared = True
+        self.options["is-wire"].shared = True
+        self.options["opencv"].shared = True
+        self.options["opencv"].with_qt = False
+
+    def build(self):
+        cmake = CMake(self, generator='Ninja')
+        cmake.definitions[
+            "CMAKE_POSITION_INDEPENDENT_CODE"] = self.options.fPIC
+        cmake.definitions["enable_tests"] = self.options.build_tests
+        cmake.configure()
+        cmake.build()
+        # if self.options.build_tests:
+        #     cmake.test()
+        # cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = ["is-cameras"]
